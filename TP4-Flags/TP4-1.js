fdea702a4c9a7cff3020a522defc06d0d7c96870
François SP <francois-sp@gmx.fr>
2024-03-04 08:46:57 +0100

    Le fichier git est au bon endroit

--- TP4-Flags/TP4-1.js
+++ TP4-Flags/TP4-1.js
@@ -1,32 +1,21 @@
 document.addEventListener('DOMContentLoaded', function (event) {
 	// Attente que le DOM soit chargé avant d'utiliser Javascript
 	// Ecrire votre code ici
-    let liste_Fr= [];
-    let liste_En= [];
-    let liste_Es= [];
-
-    let urlRecherche="https://flagcdn.com";  // Url de l'API
-    // Remplissage tableau des pays en français
-    httpRequest(urlRecherche+"/fr/codes.json",liste_Fr);
-    // Remplissage tableau des pays en anglais
-    httpRequest(urlRecherche+"/en/codes.json",liste_En);
-    // Remplissage tableau des pays en Espagnol
-    httpRequest(urlRecherche+"/es/codes.json",liste_Es);
 
     const leBoutonFr = document.getElementById("leBoutonFr");
     const leBoutonEn = document.getElementById("leBoutonEn");
     const leBoutonEs = document.getElementById("leBoutonEs");
 
     leBoutonFr.addEventListener("click", function(){
-        console.log(liste_Fr);
+        console.log(list_Fr);
     });
 
     leBoutonEn.addEventListener("click", function(){
-        console.log(liste_En);
+        console.log(list_En);
     });
 
     leBoutonEs.addEventListener("click", function(){
-        console.log(liste_Es);
+        console.log(list_Es);
     });
 
 
