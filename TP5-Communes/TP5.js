4d2585b464208c68355ff27fbab4be16e2a7eba6
François SP <francois-sp@gmx.fr>
2024-03-15 12:02:59 +0100

    TP5 Debut

---
+++ TP5-Communes/TP5.js
@@ -0,0 +1,11 @@
+$(function () {
+    const listeRegions = $('#listeRegions');
+    const listeDepartements = $('#listeDepartements');
+    const listeCommunes = $('#listeCommunes');
+
+    const urlAPI = 'https://geo.api.gouv.fr';
+    const urlRegions = urlAPI + '/regions';
+    const urlDepartements = urlAPI + '/departemens/';
+
+
+});
